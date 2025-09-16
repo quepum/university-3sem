e80bb254c2b60e07a259605b930d4e2bb60663a2
quepum <118292850+quepum@users.noreply.github.com>
2025-09-16 23:55:23 +0300

    style: fixed warnings

--- Homework1/Homework1.Tests/MultiplicationTests.cs
+++ Homework1/Homework1.Tests/MultiplicationTests.cs
@@ -10,9 +10,21 @@ namespace Homework1.Tests
         [Test]
         public void SequentialMultiplication_SmallMatrices_ReturnsCorrectResult()
         {
-            var a = new[,] { { 1, 2 }, { 3, 4 } };
-            var b = new[,] { { 2, 0 }, { 1, 2 } };
-            var expected = new[,] { { 4, 4 }, { 10, 8 } };
+            var a = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
+            var b = new[,]
+            {
+                { 2, 0 },
+                { 1, 2 },
+            };
+            var expected = new[,]
+            {
+                { 4, 4 },
+                { 10, 8 },
+            };
 
             var result = MatrixMultiplier.SequentialMultiplication(a, b);
 
@@ -22,9 +34,22 @@ namespace Homework1.Tests
         [Test]
         public void SequentialMultiplication_RectangularMatrices_ReturnsCorrectResult()
         {
-            var a = new[,] { { 1, 2, 3 }, { 4, 5, 6 } };
-            var b = new[,] { { 7, 8 }, { 9, 10 }, { 11, 12 } };
-            var expected = new[,] { { 58, 64 }, { 139, 154 } };
+            var a = new[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+            };
+            var b = new[,]
+            {
+                { 7, 8 },
+                { 9, 10 },
+                { 11, 12 },
+            };
+            var expected = new[,]
+            {
+                { 58, 64 },
+                { 139, 154 },
+            };
 
             var result = MatrixMultiplier.SequentialMultiplication(a, b);
 
@@ -34,8 +59,16 @@ namespace Homework1.Tests
         [Test]
         public void SequentialMultiplication_IdentityMatrix_ReturnsOriginalMatrix()
         {
-            var original = new[,] { { 1, 2 }, { 3, 4 } };
-            var identity = new[,] { { 1, 0 }, { 0, 1 } };
+            var original = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
+            var identity = new[,]
+            {
+                { 1, 0 },
+                { 0, 1 },
+            };
 
             var result = MatrixMultiplier.SequentialMultiplication(original, identity);
 
@@ -53,16 +86,31 @@ namespace Homework1.Tests
 
             var result = MatrixMultiplier.SequentialMultiplication(a, b);
 
-            Assert.That(result.GetLength(0), Is.EqualTo(rowsA));
-            Assert.That(result.GetLength(1), Is.EqualTo(colsB));
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.GetLength(0), Is.EqualTo(rowsA));
+                Assert.That(result.GetLength(1), Is.EqualTo(colsB));
+            });
         }
 
         [Test]
         public void SequentialMultiplication_ZeroMatrices_ReturnsZeroMatrix()
         {
-            var a = new[,] { { 0, 0 }, { 0, 0 } };
-            var b = new[,] { { 1, 2 }, { 3, 4 } };
-            var expected = new[,] { { 0, 0 }, { 0, 0 } };
+            var a = new[,]
+            {
+                { 0, 0 },
+                { 0, 0 },
+            };
+            var b = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
+            var expected = new[,]
+            {
+                { 0, 0 },
+                { 0, 0 },
+            };
 
             var result = MatrixMultiplier.SequentialMultiplication(a, b);
 
@@ -100,9 +148,21 @@ namespace Homework1.Tests
         [TestCase(8)]
         public void ParallelMultiplication_WithDifferentThreadCounts_ProducesCorrectResult(int threadCount)
         {
-            var a = new[,] { { 1, 2 }, { 3, 4 } };
-            var b = new[,] { { 2, 0 }, { 1, 2 } };
-            var expected = new[,] { { 4, 4 }, { 10, 8 } };
+            var a = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
+            var b = new[,]
+            {
+                { 2, 0 },
+                { 1, 2 },
+            };
+            var expected = new[,]
+            {
+                { 4, 4 },
+                { 10, 8 },
+            };
 
             var result = MatrixMultiplier.ParallelMultiplication(a, b, threadCount);
 
@@ -112,40 +172,72 @@ namespace Homework1.Tests
         [Test]
         public void ParallelMultiplication_WithThreadCountLessThanOrEqualToOne_UsesSequential()
         {
-            var a = new[,] { { 1, 2 }, { 3, 4 } };
-            var b = new[,] { { 2, 0 }, { 1, 2 } };
-            var expected = new[,] { { 4, 4 }, { 10, 8 } };
+            var a = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
+            var b = new[,]
+            {
+                { 2, 0 },
+                { 1, 2 },
+            };
+            var expected = new[,]
+            {
+                { 4, 4 },
+                { 10, 8 },
+            };
 
             var result1 = MatrixMultiplier.ParallelMultiplication(a, b, 0);
             var result2 = MatrixMultiplier.ParallelMultiplication(a, b, 1);
 
-            Assert.That(result1, Is.EqualTo(expected));
-            Assert.That(result2, Is.EqualTo(expected));
+            Assert.Multiple(() =>
+            {
+                Assert.That(result1, Is.EqualTo(expected));
+                Assert.That(result2, Is.EqualTo(expected));
+            });
         }
 
         [Test]
         public void SequentialMultiplication_NullFirstMatrix_ThrowsArgumentNullException()
         {
-            var b = new[,] { { 1, 2 }, { 3, 4 } };
+            var b = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
-            Assert.That(() => MatrixMultiplier.SequentialMultiplication(null!, b),
+            Assert.That(
+                () => MatrixMultiplier.SequentialMultiplication(null!, b),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         public void SequentialMultiplication_NullSecondMatrix_ThrowsArgumentNullException()
         {
-            var a = new[,] { { 1, 2 }, { 3, 4 } };
+            var a = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
-            Assert.That(() => MatrixMultiplier.SequentialMultiplication(a, null!),
+            Assert.That(
+                () => MatrixMultiplier.SequentialMultiplication(a, null!),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         public void SequentialMultiplication_IncompatibleDimensions_ThrowsArgumentException()
         {
-            var a = new[,] { { 1, 2, 3 } };
-            var b = new[,] { { 1, 2 }, { 3, 4 } };
+            var a = new[,]
+            {
+                { 1, 2, 3 },
+            };
+            var b = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
             var exception = Assert.Throws<ArgumentException>(() =>
                 MatrixMultiplier.SequentialMultiplication(a, b));
@@ -155,26 +247,43 @@ namespace Homework1.Tests
         [Test]
         public void ParallelMultiplication_NullFirstMatrix_ThrowsArgumentNullException()
         {
-            var b = new[,] { { 1, 2 }, { 3, 4 } };
+            var b = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
-            Assert.That(() => MatrixMultiplier.ParallelMultiplication(null!, b, 2),
+            Assert.That(
+                () => MatrixMultiplier.ParallelMultiplication(null!, b, 2),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         public void ParallelMultiplication_NullSecondMatrix_ThrowsArgumentNullException()
         {
-            var a = new[,] { { 1, 2 }, { 3, 4 } };
+            var a = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
-            Assert.That(() => MatrixMultiplier.ParallelMultiplication(a, null!, 2),
+            Assert.That(
+                () => MatrixMultiplier.ParallelMultiplication(a, null!, 2),
                 Throws.TypeOf<ArgumentNullException>());
         }
 
         [Test]
         public void ParallelMultiplication_IncompatibleDimensions_ThrowsArgumentException()
         {
-            var a = new[,] { { 1, 2, 3 } };
-            var b = new[,] { { 1, 2 }, { 3, 4 } };
+            var a = new[,]
+            {
+                { 1, 2, 3 },
+            };
+            var b = new[,]
+            {
+                { 1, 2 },
+                { 3, 4 },
+            };
 
             var exception = Assert.Throws<ArgumentException>(() =>
                 MatrixMultiplier.ParallelMultiplication(a, b, 2));
